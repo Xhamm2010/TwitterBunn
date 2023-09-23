8c4d9db9ff575941558e557fc793933ee4e129e3
Ezekiel <xhamm2010@gmail.com>
2023-09-23 18:03:06 +0100

    Twitter Contract

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+require('@nomicfoundation/hardhat-toolbox');
+
+/** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config();
+
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
